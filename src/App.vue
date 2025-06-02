e805278c8d21007d03930de5368e28e165c52019
azamjonbro <azamjondasturchi11@gmail.com>
2025-06-03 01:43:02 +0500

    added generic button and title

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,16 @@
 <template >
   <div>
     <div class="color-1 text-5">Assalomu alaykum</div>
+    <Button class="add-project"/>
   </div>
 </template>
 <script>
+import Button from './Components/Templates/Button/Button.vue';
+
 export default {
-  
+  components:{
+    Button
+  }
 }
 </script>
 <style >
---
+++ src/Components/Templates/Button/Button.vue
@@ -0,0 +1,23 @@
+<template >
+ <button :title="title">
+  {{ value }}
+ </button>
+</template>
+<script>
+export default {
+ props:{
+  value:{
+   type:String,
+   required:true,
+   default:"Add project"
+  },
+  title:{
+   type:String,
+   default:""
+  }
+ },
+}
+</script>
+<style >
+ 
+</style>
