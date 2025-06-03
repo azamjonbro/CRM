89fa4e076e2145623f0fe9c01786335a8aca9319
azamjonbro <azamjondasturchi11@gmail.com>
2025-06-03 23:55:34 +0500

    added

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,18 @@
   <div>
     <div class="color-1 text-5">Assalomu alaykum</div>
     <Button class="icon info" IconName="PlusIcon"/>
+    <Input placeholder="Enter problem" value="+998" :width="500" iconName="EyeOpenIcon"/>
   </div>
 </template>
 <script>
 import Button from './Components/Templates/Button/Button.vue';
+import Input from './Components/Templates/Input/Input.vue';
 
 export default {
   components:{
-    Button
+    Button,
+    Input,
+
   }
 }
 </script>
